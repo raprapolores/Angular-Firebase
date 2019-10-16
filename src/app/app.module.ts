26f1da9f31e42f2bfe2ade01974292a9def162e8
raprapolores <oloresralph@gmail.com>
2019-10-17 07:27:30 +0800

    basic components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component'
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserComponent,
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  firebase: {
+    apiKey: "AIzaSyAYKLsTzLd6cG1ilW4raADP-zvtvWDh0Ys",
+    authDomain: "finals-project-954b3.firebaseapp.com",
+    databaseURL: "https://finals-project-954b3.firebaseio.com",
+    projectId: "finals-project-954b3",
+    storageBucket: "finals-project-954b3.appspot.com",
+    messagingSenderId: "1051043315000",
+    appId: "1:1051043315000:web:d3bcf96395e9a9f96955cd",
+    measurementId: "G-VJQV41M13B"
+  }
 };
